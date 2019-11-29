d8466d62c4a41bddd3c8d716aaee8051bbe1aee8
KlausChan03 <KlausChan03@gmail.com>
2019-11-29 17:42:12 +0800

    添加json导出excel功能及其相关；

--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="app-container">
+    <el-form>
+      <el-form-item>
+        <el-button  size="mini" @click="export2Excel">导出</el-button>
+      </el-form-item>
+    </el-form>
     <el-table
       v-loading="listLoading"
       :data="list"
@@ -9,19 +14,13 @@
       highlight-current-row
     >
       <el-table-column align="center" label="ID" width="95">
-        <template slot-scope="scope">
-          {{ scope.$index }}
-        </template>
+        <template slot-scope="scope">{{ scope.$index }}</template>
       </el-table-column>
       <el-table-column label="Title">
-        <template slot-scope="scope">
-          {{ scope.row.title }}
-        </template>
+        <template slot-scope="scope">{{ scope.row.title }}</template>
       </el-table-column>
       <el-table-column label="Birthday">
-        <template slot-scope="scope">
-          {{ scope.row.birthday }}
-        </template>
+        <template slot-scope="scope">{{ scope.row.birthday }}</template>
       </el-table-column>
       <el-table-column label="Author" width="110" align="center">
         <template slot-scope="scope">
@@ -29,9 +28,7 @@
         </template>
       </el-table-column>
       <el-table-column label="Pageviews" width="110" align="center">
-        <template slot-scope="scope">
-          {{ scope.row.pageviews }}
-        </template>
+        <template slot-scope="scope">{{ scope.row.pageviews }}</template>
       </el-table-column>
       <el-table-column class-name="status-col" label="Status" width="110" align="center">
         <template slot-scope="scope">
@@ -50,40 +47,69 @@
 </template>
 
 <script>
-import { getList } from '@/api/table'
+import { getList } from "@/api/table";
 // import _ from 'lodash'
 export default {
   filters: {
     statusFilter(status) {
       const statusMap = {
-        published: 'success',
-        draft: 'gray',
-        deleted: 'danger'
-      }
-      return statusMap[status]
+        published: "success",
+        draft: "gray",
+        deleted: "danger"
+      };
+      return statusMap[status];
     }
   },
   data() {
     return {
       list: null,
       listLoading: true
-    }
+    };
   },
   created() {
-    this.fetchData()
+    this.fetchData();
   },
   methods: {
     fetchData() {
-      console.log(getList)
-      this.listLoading = true
+      this.listLoading = true;
       getList().then(response => {
-        console.log(response.data.items)
-        const _first = this._.head(response.data.items)
-        const _last = this._.last(response.data.items)
-        const _all = this._.concat([_first], [_last])
-        this.list = _all
-        console.log(_first)
-        this.listLoading = false
+        // console.log(response.data.items);
+        // const _first = this._.head(response.data.items);
+        // const _last = this._.last(response.data.items);
+        // const _all = this._.concat([_first], [_last]);
+        // this.list = _all;
+        this.list = response.data.items;
+        this.listLoading = false;
+      });
+    },
+    formatJson(filterVal, jsonData) {
+      return jsonData.map(v => filterVal.map(j => v[j]));
+    },
+    export2Excel() {
+      console.log(this.list,"i am here")
+      require.ensure([], () => {
+        const { export_json_to_excel } = require("../../vendor/Export2Excel");
+        const tHeader = [
+          "ID",
+          "Title",
+          "Birthday",
+          "Author",
+          "Pageviews",
+          "Status",
+          "Display_time",
+        ];
+        const filterVal = [
+          "id",
+          "title",
+          "birthday",
+          "author",
+          "pageviews",
+          "status",
+          "display_time",
+        ];
+        const list = this.list;
+        const data = this.formatJson(filterVal, list);
+        export_json_to_excel(tHeader, data, "普通数据表格");
       })
     }
   }
